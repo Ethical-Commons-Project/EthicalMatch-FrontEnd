2b55d3642263febf44a25683957c91ae4773f4db
Ethan Gunter <egunter.tech@gmail.com>
2024-11-27 14:34:15 -0500

    Un-uglified

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,37 @@
-<h1>Welcome to the Ethical Match Web app!</h1>
-<p>You're quite early in the development process, so here are some good places to get started</p>
-<ul>
-    <li>Contribute to the documentation for <a href="https://github.com/orgs/Ethical-Commons-Project/projects/2/views/5?query=sort%3Aupdated-desc+is%3Aopen">features</a> that are in the drafting process</li>
-    <li>Fix any <a href="https://github.com/Ethical-Commons-Project/EthicalMatch-FrontEnd/issues">outstanding issues</a> in the frontend code</li>
-    <li>Join the <a href="https://discord.gg/P7qfVuqMXz">Discord</a> and participate in the conversation there</li>
-</ul>
+<script>
+	import '$style/app.scss';
+</script>
+
+<div id="app">
+	<div>
+		<h1>Welcome to the Ethical Match Web app!</h1>
+		<p>
+			You're <em>very</em> early in the development process, so here are some good places to get started
+		</p>
+		<br />
+		<ul>
+			<li>
+				Contribute to the documentation for <a
+					href="https://github.com/orgs/Ethical-Commons-Project/projects/2/views/5?query=sort%3Aupdated-desc+is%3Aopen"
+					>features</a
+				> that are in the drafting process
+			</li>
+			<li>
+				Implement solutions for <a
+					href="https://github.com/orgs/Ethical-Commons-Project/projects/2/views/7?query=sort%3Aupdated-desc+is%3Aopen"
+					>features</a
+				>
+				that <em>have</em> been designed and documented
+			</li>
+			<li>
+				Fix any <a href="https://github.com/Ethical-Commons-Project/EthicalMatch-FrontEnd/issues"
+					>outstanding issues</a
+				> in the frontend code
+			</li>
+			<li>
+				Join the <a href="https://discord.gg/P7qfVuqMXz">Discord</a> and participate in the conversation
+				there
+			</li>
+		</ul>
+	</div>
+</div>
